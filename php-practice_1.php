90b3c4f89dba5f8022d23725f6f1b803e8f9ca42
jumpei hirosawa <jumpeihirosawa@jumpeinoMacBook-Pro.local>
2025-08-20 06:54:03 +0900

    add question answer

--- php-practice_1.php
+++ php-practice_1.php
@@ -3,11 +3,15 @@
 $name = '「安藤」';
 echo '私の名前は'  . $name . 'です。';
 
+// $は変数の頭につく。//
+//.変数との結合。//
+
 // Q2 四則演算
 $num = 5*4;
 echo $num . "\n";
 echo $num / 2;
 
+// "\n"は改行を意味する。//
 // Q3 日付操作
 date_default_timezone_set('Asia/Tokyo');
 echo '現在時刻は、' date('Y年m月d日 H時i分s秒') 'です';
@@ -25,6 +29,9 @@ echo '使用OSは、windowsです。';
 } 
 }
 
+//ifは条件を提示し、elseではfalseの時の結果を表示する。文法;の後に続く場合は{}で括る//
+//elseifでは条件分岐を意味しどちらにも当てはまらない時に使える。//
+
 // Q5 条件分岐-2 三項演算子
 $age = 18;
 
@@ -34,9 +41,13 @@ if ($age <= 18) {
     echo '「成人です。」';
 }
 
+// <= $変数が１８未満であることを指す//
+
 // Q6 配列
 $arr = ['東京都','千葉県','埼玉県','栃木県','千葉県','茨城県','群馬県','神奈川県'];
 echo $arr[3] .'と'. $arr[4] . 'は関東地方の都道府県です';
+
+//羅列する際には$arrを使い、頭から番号は0である。//
 
 // Q7 連想配列-1
 $kanto = [
@@ -99,14 +110,57 @@ function greet($name = "ゲスト") {
 greet();        // ゲスト さん、こんにちわ。
 greet("てんとう虫");  // 花子 さん、こんにちわ。
 
-
 // Q11 関数-2
+function calcTaxInPrice($price) {
+    $taxInPrice = $price * 1.1;
+    return $taxInPrice;
+}
 
+    $price = 1000;
+    $taxInPrice = calctaxInPrice($price);
+    echo $price . "円の商品の税込価格は" . $taxInPrice . "です。";
+?>
 
 // Q12 関数とif文
+function distinguishNum($num) {
+    if ($num % 2 == 0) {
+   return $num. "は偶数です。";
+    }
+    else {
+   return $num. "は奇数です。";
+    }
+}
 
+echo distinguishNum(13);
+echo "\n";
+echo distinguishNum(26);
+
+?>
 
 // Q13 関数とswitch文
+function evaluateGrade($grade) {
 
+switch ($grade) {
+    case "A":
+    case "B":
+    return "合格です。";
+    //break;//
+    
+    case "C":
+    return "合格ですが追加課題があります。";
+    //break;//
+    
+    case "D":
+    return "不合格です";
+    //break;//
+    
+    default:
+    return "判定不明です。講師に問い合わせてください。";
+    //break;//
+}
+}   
+    echo evaluateGrade("A","B");
+    echo "\n";
+    echo evaluateGrade("G");
 
 ?>
--- php-practice_2.php
+++ php-practice_2.php
@@ -1,15 +1,150 @@
 <?php
 // Q1 tic-tac問題
+echo "1から100までのカウントを開始します\n";
 
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 4 == 0 && $i % 5 == 0) {
+        echo "tic-tac\n";
+    } elseif ($i % 4 == 0) {
+        echo "tic\n";
+    } elseif ($i % 5 == 0) {
+        echo "tac\n";
+    } else {
+        echo $i . "\n";
+    }
+}
 
 // Q2 多次元連想配列
+//１問目//
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+  ];  
+    echo $personalInfos [1] ['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
+
+    //２問目//
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+  ];  
+    foreach ($personalInfos as $index => $personal) {
+    echo ($index+1) . '番目の' . $personal['name'] . 'のメールアドレスは' . $personal['mail'] . 'で、電話番号は' . $personal['tel'] . "\n";
+}
+
+//３問目//
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+  ];  
+  
+  $ageList = [25, 30, 18];
+    foreach ($personalInfos as $index => $personal) {
+        $personal['age'] = $ageList[$index];
+    }
+    var_dump($personalInfos);
 
 
 // Q3 オブジェクト-1
+<?php
+
+class Student
+{
+    public $studentId;
+    public $studentName;
+
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    public function attend()
+    {
+        echo '授業に出席しました。';
+    }
+}
+
+// インスタンス作成
+$student1 = new Student(120, "山田");
+
+// 出力
+echo '学籍番号' . $student1->studentId . '番の生徒は' . $student1->studentName . 'です。';
+
 
 
 // Q4 オブジェクト-2
+<?php
+
+class Student
+{
+    public $studentId;
+    public $studentName;
+
+    public function __construct($id, $name)
+    {
+        $this->studentId = $id;
+        $this->studentName = $name;
+    }
+
+    // attendメソッドだけ修正
+    public function attend($subject)
+    {
+        echo '学籍番号' . $this->studentId . '番の' . $this->studentName . 'は' . $subject . 'の授業に出席しました。';
+    }
+}
+
+$yamada = new Student(120, '山田');
+$yamada->attend('PHP');
+
 
 
 // Q5 定義済みクラス
-?>
+１問目
+$dt = new DateTime(); 
+$dt->modify('2021-02-02');
+echo $dt->format('Y-m-d');
+
+２問目
+$today = new DateTime();
+$thatSDay = new DateTime('1992-04-25');
+$diff = $today->diff($thatDay);
+echo "あの日から" . $diff->format('%a') . "日経過しました。";
